6bd472a43c63fb47ce1534a02617c3a935450892
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-01 22:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function mincost(arr)
 // return the min cost
   let res=0;
 	while(arr.length>1){
-		arr.sort();
+		 arr.sort((a, b) => a - b); 
 		sum+=arr[0]+arr[1];
 		arr[1]=arr[0]+arr[1];
 		arr.slice(1,arr.length-1);
